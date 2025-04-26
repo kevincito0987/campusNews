4bc6b823a8908cbfc68ae8bc45adc2efbba6e28a
kevincito0987 <kevinangaritacaamano@gmail.com>
2025-04-26 15:40:04 -0500

    perf: :zap: primera parte del componente.

--- src/components/filters.js
+++ src/components/filters.js
@@ -1,1 +1,9 @@
 console.log("Funcion filters"); // 🎯 Punto de partida para verificar la función
+
+
+
+document.addEventListener("campus:category-change", (event) => {
+    console.log("🚀 Evento capturado:", event.detail.category);
+    // 🎯 Lógica personalizada para la categoría seleccionada
+});
+
