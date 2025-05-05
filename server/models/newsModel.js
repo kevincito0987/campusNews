e5109cf708fadd86f46296af7a0536d080cffed7
kevincito0987 <kevinangaritacaamano@gmail.com>
2025-05-05 08:15:30 -0500

    perf: :zap: I've created news models.

---
+++ server/models/newsModel.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const NewsSchema = new mongoose.Schema({
+    source: { 
+        id: {type: String, required: true},
+        name: {type: String, required: true},
+    },
+    author: {type: String, required: true},
+    title: {type: String, required: true},
+    description: {type: String, required: true},
+    url: {type: String, required: true},
+    urlToImage: {type: String, required: true},
+    publishedAt: {type: String, required: true},
+    content: {type: String, required: true},
+    content: {type: String, required: true}
+});
+
+const News = mongoose.model("News", NewsSchema);
+module.exports = News;
